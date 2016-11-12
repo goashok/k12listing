0134b71dd84834407ad122ab9538ad4d0b0b5b8d
goashok <goashok@gmail.com>
2016-11-12 11:02:21 -0500

    sending 2 emails with different subj

--- Contact.py
+++ Contact.py
@@ -30,18 +30,26 @@ class ContactSeller:
             appSession.flash("error", "You need to be signed in to contact the seller")
             raise web.redirect(referer)
     	i = web.input()
-    	params = dict(postedby=i.userid)
-    	users = util.db.select('users', params, where='id = $postedby')
+    	params = dict(seller=i.userid)
+    	users = util.db.select('users', params, where='id = $seller')
     	if not users:
             appSession.flash("error", "User {} Not found".format(i.username))
             return web.redirect(referer)
-        postedby_user = users[0]
-        subject = "Intersted buyer for your listing on K12Listing.com for {} - {}".format(i.itemtype, i.itemtitle)
-        comment = i.comment + """
-        Please reply to user  {} directly. DO NOT reply to k12listing@gmail.com.
-        """.format(web.ctx.session.useremail)
-        mail.send(postedby_user.email, subject, comment, cc=web.ctx.session.useremail)
-        appSession.flash("success", "Sent email to user. You have been cc-ed on the email sent to seller")
+        seller = users[0]
+        buyer = web.ctx.session
+        seller_subject = "Intersted buyer for your listing on K12Listing.com for {} - {}".format(i.itemtype, i.itemtitle)
+        seller_comment =  """
+        User {} is trying to contact you for your listing '{}'. Please reply to user  {} directly. Please do not reply to k12listing@gmail.com.
+        Comment from buyer:
+        {}
+        """.format(buyer.username, i.itemtitle, buyer.useremail, i.comment)
+        buyer_subject = "Your interest in K12Listing for {} - {}".format(i.itemtype, i.itemtitle)
+        buyer_comment = """
+        We have sent email to the seller about your interest. You can contact the seller directly at {}.
+        Hope your kids enjoy the {}""".format(seller.email, i.itemtype)
+        mail.send(seller.email, seller_subject, seller_comment)
+        mail.send(buyer.useremail, buyer_subject, buyer_comment)
+        appSession.flash("success", "Sent email to the seller. We also sent you the seller's contacts")
         raise web.redirect(referer)
 
 
