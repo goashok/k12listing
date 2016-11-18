b2f07c14fe9f1320646d6601924be2f1a7c0d1bc
goashok <goashok@gmail.com>
2016-11-17 23:27:12 -0500

    changed listing view added contact seller

--- Instruments.py
+++ Instruments.py
@@ -21,7 +21,7 @@ appSession = AppSession()
 
 class InstrumentFind:
     def GET(self):
-        query = """select *,i.id as instrumentid, to_char(i.created, 'YYYY-MM-DD HH:MI:SS AM') as upload_time from instruments i, users u where i.userid=u.id order by i.created desc limit 200"""
+        query = """select *,i.id as instrumentid, to_char(i.created, 'YYYY-MM-DD') as upload_time from instruments i, users u where i.userid=u.id order by i.created desc limit 200"""
         instrument_result = util.db.query(query)
         instruments = list(instrument_result)
         for instrument in instruments:
@@ -37,10 +37,10 @@ class InstrumentFind:
             city = i.location.split(',')[0].strip()
             state = i.location.split(',')[1].strip()
             print("Location>> [{}] [{}]".format(city, state))
-            query = """select *, i.id as instrumentid, to_char(i.created, 'YYYY-MM-DD HH:MI:SS AM') as upload_time  from instruments i, users u where i.userid=u.id and (i.title ilike $term or i.instrument ilike $term) and u.city ilike $city and u.state ilike $state order by i.created desc limit 200"""
+            query = """select *, i.id as instrumentid, to_char(i.created, 'YYYY-MM-DD') as upload_time  from instruments i, users u where i.userid=u.id and (i.title ilike $term or i.instrument ilike $term) and u.city ilike $city and u.state ilike $state order by i.created desc limit 200"""
             params = dict(term=term, city=city, state=state)
         else:
-            query = """select *, i.id as instrumentid, to_char(i.created, 'YYYY-MM-DD HH:MI:SS AM') as upload_time  from instruments i, users u where i.userid=u.id and (i.title ilike $term or i.instrument ilike $term) order by i.created desc limit 200"""
+            query = """select *, i.id as instrumentid, to_char(i.created, 'YYYY-MM-DD') as upload_time  from instruments i, users u where i.userid=u.id and (i.title ilike $term or i.instrument ilike $term) order by i.created desc limit 200"""
             params = dict(term=term)
         instrument_result = util.db.query(query, vars=params)
         instruments = list(instrument_result)
